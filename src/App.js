ff0ce5e41f292eb2f26ab7c93aeff30dcbe94903
iRonic <mariocraciundaniel@gmail.com>
2026-02-28 17:22:23 +0200

    update app
    -in my app i added useEffect to fetch all the data from data.json and populate the html
    -also updating the route functionality updated home path and also the element of home contains now the prop to connect to the home page and also cards
    -details path added dinamyc part where i use :id to check after that in details if the id is correctly and to show the exact details from that id

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.scss';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
+import './App.scss';
 import Home from './pages/Home'
 import About from './pages/About'
+import Details from './pages/Details';
 import Navbar from './components/Navbar';
 
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/data/data.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <div className="header">
@@ -13,11 +26,13 @@ function App() {
       </div>
       <div className="main">
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home data={data} />} />
           <Route path="/About" element={<About />} />
+          <Route path="/Details/:id" element={<Details data={data} />} />
         </Routes>
       </div>
-      <div className="footer"></div>
+      <div className="footer">
+      </div>
     </>
   );
 }
