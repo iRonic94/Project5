4eef19081d22a1b48360e5766df139fe32824c66
iRonic <mariocraciundaniel@gmail.com>
2026-03-05 19:22:05 +0200

    update home page
    -updating the home page to use the helper file to get the data from api.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import Cards from "../components/Cards";
 import Banner from "../components/Banner";
+import { getLocations } from '../api/api';
 
-function Home({ data }) {
+function Home() {
+    const data = getLocations();
     return <div className="home">
         <Banner />
         <Cards data={data} />
