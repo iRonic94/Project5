c263eac291afda1d81d94364b59b55ffecb183b3
iRonic <mariocraciundaniel@gmail.com>
2026-03-11 21:59:03 +0200

    update dropdown
    -now the dropdown will take two props one for the title and one for the text that can reused anywhere on the pages

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import arrow from '../assets/arrow-up.svg';
-function Dropdown() {
+function Dropdown({ title, text }) {
     const [show, setShow] = useState(false);
     const showContent = () => {
         setShow(prev => !prev);
@@ -8,11 +8,11 @@ function Dropdown() {
     return (
         <div className="dropdown">
             <div className="dropdown-header" >
-                <span>title</span>
+                <span>{title}</span>
                 <span className={`arrow ${show ? "open" : ""}`} onClick={showContent}><img src={arrow} alt="arrow"></img></span>
             </div>
             <div className={`dropdown-content ${show ? "show" : ""}`}>
-                text
+                {text}
             </div>
         </div>
     );
