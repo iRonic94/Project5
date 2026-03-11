206a0656e44c7505540c59192808b42024d7ba8c
iRonic <mariocraciundaniel@gmail.com>
2026-03-11 22:01:21 +0200

    update about page
    updated the about page by adding the rest of the elements and text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,31 @@ import Banner from '../components/Banner';
 import Dropdown from '../components/Dropdown';
 
 function About() {
+    const aboutArr = [
+        {
+            title: "Reliability",
+            text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        },
+        {
+            title: "Respect",
+            text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        },
+        {
+            title: "Service",
+            text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        },
+        {
+            title: "Security",
+            text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        }
+    ];
+
     return <div className="about">
         <Banner banner={bannerAbout} />
-        <Dropdown />
-        <Dropdown />
-        <Dropdown />
-        <Dropdown />
+        {aboutArr.map(item => (
+            <Dropdown title={item.title} text={item.text} />
+        ))}
+
     </div>
 }
 export default About;
