953ef163f4aebca790edd8572489e21816e4625a
iRonic <mariocraciundaniel@gmail.com>
2026-03-11 21:57:24 +0200

    updating the fetch functionality
    -now instead of storing the fetch into a variable as a promise i will always fetch and using them inside of the scope to get the latest version of the json file

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
-let data = fetch('/data/data.json').then(res => res.json());
-
 export function getLocations() {
-    return data
+    return fetch('/data/data.json').then(res => res.json());
 }
 
 export function getLocation(id) {
+    let data = fetch('/data/data.json').then(res => res.json());
     return data.then(val => val.find(place => place.id === id));
 }
