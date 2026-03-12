6b4c7177d47e09482ff1dc8e5edca78c14fd947e
iRonic <mariocraciundaniel@gmail.com>
2026-03-12 02:16:42 +0200

    correcting a mistake

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 function Error() {
     return (
-        <div class="error-container">
+        <div className="error-container">
             <span className="error-number">404</span>
             <span className="error-message">Oops! The page you’re looking for doesn’t exist.</span>
             <span className="redirect-home"><NavLink to="/">Return to home page</NavLink></span>
