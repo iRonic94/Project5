82d6a68ed681fed38ccfec8437b9969122ce3f4f
iRonic <mariocraciundaniel@gmail.com>
2026-03-16 19:21:10 +0200

    update api
    adding extra steps for the promise to catch the error

--- src/api/api.js
+++ src/api/api.js
@@ -4,5 +4,11 @@ export function getLocations() {
 
 export function getLocation(id) {
     let data = fetch('/data/data.json').then(res => res.json());
-    return data.then(val => val.find(place => place.id === id));
+    return data.then(val => {
+        const location = val.find(place => place.id === id);
+        if (!location) {
+            throw new Error("We didn't find the location!");
+        }
+        return location;
+    });
 }
